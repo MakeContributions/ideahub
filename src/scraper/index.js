87078fec2f10e94de0b0cc36860aeeb1c5a64e75
Ming Tsai <ming.jia1213@gmail.com>
2021-05-07 19:16:31 -0400

    fix: the group by is not return the data

--- src/scraper/index.js
+++ src/scraper/index.js
@@ -8,7 +8,8 @@ const pattern = new RegExp(`${start_comment}[\\s\\S]*${end_comment}`, 'gm');
 const groupBy = (list, key) => {
   return list.reduce(function (rv, x) {
     const val = rv[x[key]];
-    rv[x[key]] = val ? val : [];
+    rv[x[key]] = val|| [];
+    (rv[x[key]]).push(x);
     return rv;
   }, {});
 };
@@ -27,7 +28,7 @@ const capitalize = (s) => {
 };
 
 const wordCapitalize = (s) => {
-  const text = s.split(' ').filter((t) => t !== '');
+  const text = s.split(' ').filter(t => t !== '');
   return text.map((t) => capitalize(t)).join(' ');
 };
 
@@ -35,48 +36,44 @@ const categories = groupBy(data, 'category');
 
 const writeToFile = (text) => {
   fs.readFile(readme, 'utf8', function (err, context) {
-    if (err) {
-      return console.log(err);
-    }
-    const replacement = `${start_comment}\n${text}\n${end_comment}`;
-    var newContext = context.replace(pattern, replacement);
+      if (err) {
+          return console.log(err);
+      }
+      const replacement = `${start_comment}\n${text}\n${end_comment}`;
+      var newContext = context.replace(pattern, replacement);
 
-    fs.writeFileSync(readme, newContext, 'utf8');
+      fs.writeFileSync(readme, newContext, 'utf8');
   });
 };
 
-let categoriesText = '';
-Object.keys(categories)
-  .sort()
-  .forEach((c) => {
-    const list = categories[c];
-    const levels = groupBy(list, 'level');
-    const sorted = Object.keys(levels).sort((a, b) => sortBy(a, b));
-    const filePrefix = c.toUpperCase().replace(/ /, '_');
-    const mapped = sorted.map((s) => {
-      return `[${wordCapitalize(
-        s
-      )}](./docs/${filePrefix}_${s.toUpperCase()}.md)`;
-    });
-    const category = wordCapitalize(c);
-    categoriesText += `- ${category}\n    - ${mapped.join('\n    - ')}\n`;
-    sorted.forEach((l) => {
-      const project = levels[l]
-        .map((p) => {
-          return (
-            `  - [${p.display}](https://github.com/${p.author}/${p.name}) - ` +
-            `[@${p.author}](https://github.com/${p.author})`
-          );
-        })
-        .join('\n');
-      const level = `${capitalize(l)}`;
-      const current = `# ${category} - ${level}\n${project}`;
-      fs.writeFileSync(
-        `./docs/${filePrefix}_${l.toUpperCase()}.md`,
-        current,
-        'utf-8'
-      );
-    });
+let  categoriesText = '';
+Object.keys(categories).sort().forEach((c) => {
+  const list = categories[c];
+  const levels = groupBy(list, 'level');
+  const sorted = Object.keys(levels).sort((a, b) => sortBy(a, b));
+  const filePrefix = c.toUpperCase().replace(/ /, '_');
+  const mapped = sorted.map((s) => {
+    return `[${wordCapitalize(s)}](./docs/${filePrefix}_${s.toUpperCase()}.md)`;
   });
+  const category = wordCapitalize(c);
+  categoriesText += `- ${category}\n    - ${mapped.join('\n    - ')}\n`;
+  sorted.forEach((l) => {
+    const project = levels[l]
+      .map((p) => {
+        return (
+          `  - [${p.display}](https://github.com/${p.author}/${p.name}) - ` +
+          `[@${p.author}](https://github.com/${p.author})`
+        );
+      })
+      .join('\n');
+    const level = `${capitalize(l)}`;
+    const current = `# ${category} - ${level}\n${project}`;
+    fs.writeFileSync(
+      `./docs/${filePrefix}_${l.toUpperCase()}.md`,
+      current,
+      'utf-8'
+    );
+  });
+});
 
 writeToFile(categoriesText);
